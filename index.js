dcc7808c5e9dd39638bb23ba86dcbae58d773598
moses ngige <moses32ngigz@gmail.com>
2022-06-12 01:47:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const testVar = "hi"
+
 document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Moses is the champion!";
 newHeader.className = "victory";
+newHeader.innerHTML = "Moses is the champion!";
